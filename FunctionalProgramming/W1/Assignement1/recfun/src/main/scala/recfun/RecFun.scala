aedbb2a6f4a5d4cb057f36191b0f9935853ebab9
angievig <angievig@hedwig.local>
2022-07-01 16:22:37 -0500

    Week1

---
+++ FunctionalProgramming/W1/Assignement1/recfun/src/main/scala/recfun/RecFun.scala
@@ -0,0 +1,31 @@
+package recfun
+
+object RecFun extends RecFunInterface:
+
+  def main(args: Array[String]): Unit =
+    println("Pascal's Triangle")
+    for row <- 0 to 10 do
+      for col <- 0 to row do
+        print(s"${pascal(col, row)} ")
+      println()
+
+  /**
+   * Exercise 1
+   */
+  def pascal(c: Int, r: Int): Int = ???
+  size = r + 1
+
+  def generateRow(r: Int): List[Int] =
+  
+    if r==0 then List(1) else Concat(1 ,generateRow(r-1))
+
+
+  /**
+   * Exercise 2
+   */
+  def balance(chars: List[Char]): Boolean = ???
+
+  /**
+   * Exercise 3
+   */
+  def countChange(money: Int, coins: List[Int]): Int = ???
